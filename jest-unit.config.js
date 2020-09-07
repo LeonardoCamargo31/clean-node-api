69cac378112d41193d9771641077ffd654822bb6
Leonardo Camargo <leonardo_camargo31@hotmail.com>
2020-09-07 19:41:19 -0300

    feat: ensure DbAddAccount calls Encrypter with correct password

--- jest-unit.config.js
+++ jest-unit.config.js
@@ -1,4 +1,4 @@
 const config = require('./jest.config')
 // qualquer arquivos com .spec.js, vou rodar essa configuração
-config.testMatch = ['**/*.spec.js']
+config.testMatch = ['**/*.spec.ts']
 module.exports = config
